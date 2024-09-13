a3d277815d24c8ba5b90ecbe4ab9a4466f28d7d8
Andrew Van Horn <andrew.van.horn@duke.edu>
2024-09-13 14:52:52 -0400

    modified dax setup experiment to work properly

--- experiments/dax_setup.py
+++ experiments/dax_setup.py
@@ -1,27 +1,27 @@
 from demo_system.system import *
+from dax.util.sub_experiment import SubExperiment
 
+from repository.dax.util.dax.dax_init import DaxInit
+from repository.dax.util.dataset_config.system_config import SystemConfig
 
 class DaxSetup(DemoSystem, Experiment):
-    """DaxSetup"""
+    """Dax Setup"""
 
     def __init__(self, managers, *args, **kwargs):
         # Capture the managers before passing them to super
         self._managers = managers
         super(DaxSetup, self).__init__(managers, *args, **kwargs)
 
-    def build(self):
-        # Call super
-        super(DaxSetup, self).build()
+    def build(self) -> None:
+        try:
+            # Build system
+            super(DaxSetup, self).build()
+        except KeyError:
+            # Could not find key and system config might not be available, fallback on defaults
+            self.logger.error('No current configuration exists yet, run this utility once to set a configuration')
+            mon_pmt_enabled = False
 
     def run(self):
-        # Set Mon PMT Enabled Key
-        self.set_dataset_sys(self.MON_PMT_ENABLED_KEY, True)
-
-        # Initialize Dax
-        self.dax_init()
-
-
-if __name__ == '__main__':
-    from artiq.frontend.artiq_run import run
-
-    run()
+        sub_experiment = SubExperiment(self, self._managers)
+        sub_experiment.run(SystemConfig, "config")
+        sub_experiment.run(DaxInit, "init")
