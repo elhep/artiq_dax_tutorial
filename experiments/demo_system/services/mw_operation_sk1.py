eae3f6c86b4052981acca9d0eeaf03d210a6047f
Andrew Van Horn <andrew.van.horn@duke.edu>
2024-09-19 12:08:37 -0400

    Added dax program

--- experiments/demo_system/services/mw_operation_sk1.py
+++ experiments/demo_system/services/mw_operation_sk1.py
@@ -18,23 +18,23 @@ class MicrowaveOperationSK1Service(MicrowaveOperationService):
         super(MicrowaveOperationSK1Service, self).init()
 
         # Pre-calculate machine units
-        self._sk1_pi_2_x: np.int32 = np.int32(self._microwave.turns_to_pow(self._SK1_PI_2))
-        self._msk1_pi_2_x: np.int32 = np.int32(self._microwave.turns_to_pow(-self._SK1_PI_2))
-        self._sk1_pi_x: np.int32 = np.int32(self._microwave.turns_to_pow(self._SK1_PI))
-        self._msk1_pi_x: np.int32 = np.int32(self._microwave.turns_to_pow(-self._SK1_PI))
+        self._sk1_pi_2_x: np.int32 = np.int32(self._microwave._dds.turns_to_pow(self._SK1_PI_2))
+        self._msk1_pi_2_x: np.int32 = np.int32(self._microwave._dds.turns_to_pow(-self._SK1_PI_2))
+        self._sk1_pi_x: np.int32 = np.int32(self._microwave._dds.turns_to_pow(self._SK1_PI))
+        self._msk1_pi_x: np.int32 = np.int32(self._microwave._dds.turns_to_pow(-self._SK1_PI))
 
-        self._sk1_pi_2_y: np.int32 = np.int32(self._microwave.turns_to_pow(0.25 + self._SK1_PI_2))
-        self._msk1_pi_2_y: np.int32 = np.int32(self._microwave.turns_to_pow(0.25 - self._SK1_PI_2))
-        self._sk1_pi_y: np.int32 = np.int32(self._microwave.turns_to_pow(0.25 + self._SK1_PI))
-        self._msk1_pi_y: np.int32 = np.int32(self._microwave.turns_to_pow(0.25 - self._SK1_PI))
+        self._sk1_pi_2_y: np.int32 = np.int32(self._microwave._dds.turns_to_pow(0.25 + self._SK1_PI_2))
+        self._msk1_pi_2_y: np.int32 = np.int32(self._microwave._dds.turns_to_pow(0.25 - self._SK1_PI_2))
+        self._sk1_pi_y: np.int32 = np.int32(self._microwave._dds.turns_to_pow(0.25 + self._SK1_PI))
+        self._msk1_pi_y: np.int32 = np.int32(self._microwave._dds.turns_to_pow(0.25 - self._SK1_PI))
 
-        self._sk1_mpi_2_x: np.int32 = np.int32(self._microwave.turns_to_pow(self._SK1_MPI_2))
-        self._msk1_mpi_2_x: np.int32 = np.int32(self._microwave.turns_to_pow(-self._SK1_MPI_2))
-        self._sk1_mpi_2_y: np.int32 = np.int32(self._microwave.turns_to_pow(0.25 + self._SK1_MPI_2))
-        self._msk1_mpi_2_y: np.int32 = np.int32(self._microwave.turns_to_pow(0.25 - self._SK1_MPI_2))
+        self._sk1_mpi_2_x: np.int32 = np.int32(self._microwave._dds.turns_to_pow(self._SK1_MPI_2))
+        self._msk1_mpi_2_x: np.int32 = np.int32(self._microwave._dds.turns_to_pow(-self._SK1_MPI_2))
+        self._sk1_mpi_2_y: np.int32 = np.int32(self._microwave._dds.turns_to_pow(0.25 + self._SK1_MPI_2))
+        self._msk1_mpi_2_y: np.int32 = np.int32(self._microwave._dds.turns_to_pow(0.25 - self._SK1_MPI_2))
 
-        self._sk1_pi_h: np.int32 = np.int32(self._microwave.turns_to_pow(0.125 + self._SK1_PI))
-        self._msk1_pi_h: np.int32 = np.int32(self._microwave.turns_to_pow(0.125 - self._SK1_PI))
+        self._sk1_pi_h: np.int32 = np.int32(self._microwave._dds.turns_to_pow(0.125 + self._SK1_PI))
+        self._msk1_pi_h: np.int32 = np.int32(self._microwave._dds.turns_to_pow(0.125 - self._SK1_PI))
 
         self.update_kernel_invariants(
             '_sk1_pi_2_x', '_msk1_pi_2_x', '_sk1_pi_x', '_msk1_pi_x',
@@ -47,44 +47,44 @@ class MicrowaveOperationSK1Service(MicrowaveOperationService):
 
     @kernel
     def x(self, qubit: TInt32 = -1):
-        self._pulse_mu(self._duration_pi_mu, 0)
-        self._pulse_mu(self._duration_pi_mu << 1, self._sk1_pi_x)
-        self._pulse_mu(self._duration_pi_mu << 1, self._msk1_pi_x)
+        self._pulse_mu(self._duration_pi_mu(), 0)
+        self._pulse_mu(self._duration_pi_mu() << 1, self._sk1_pi_x)
+        self._pulse_mu(self._duration_pi_mu() << 1, self._msk1_pi_x)
 
     @kernel
     def y(self, qubit: TInt32 = -1):
-        self._pulse_mu(self._duration_pi_mu, self._POW_PI >> 1)
-        self._pulse_mu(self._duration_pi_mu << 1, self._sk1_pi_y)
-        self._pulse_mu(self._duration_pi_mu << 1, self._msk1_pi_y)
+        self._pulse_mu(self._duration_pi_mu(), self._POW_PI >> 1)
+        self._pulse_mu(self._duration_pi_mu() << 1, self._sk1_pi_y)
+        self._pulse_mu(self._duration_pi_mu() << 1, self._msk1_pi_y)
 
     @kernel
     def sqrt_x(self, qubit: TInt32 = -1):
-        self._pulse_mu(self._duration_pi_mu >> 1, 0)
-        self._pulse_mu(self._duration_pi_mu << 1, self._sk1_pi_2_x)
-        self._pulse_mu(self._duration_pi_mu << 1, self._msk1_pi_2_x)
+        self._pulse_mu(self._duration_pi_mu() >> 1, 0)
+        self._pulse_mu(self._duration_pi_mu() << 1, self._sk1_pi_2_x)
+        self._pulse_mu(self._duration_pi_mu() << 1, self._msk1_pi_2_x)
 
     @kernel
     def sqrt_x_dag(self, qubit: TInt32 = -1):
-        self._pulse_mu(self._duration_pi_mu >> 1, self._POW_PI)
-        self._pulse_mu(self._duration_pi_mu << 1, self._sk1_mpi_2_x)
-        self._pulse_mu(self._duration_pi_mu << 1, self._msk1_mpi_2_x)
+        self._pulse_mu(self._duration_pi_mu() >> 1, self._POW_PI)
+        self._pulse_mu(self._duration_pi_mu() << 1, self._sk1_mpi_2_x)
+        self._pulse_mu(self._duration_pi_mu() << 1, self._msk1_mpi_2_x)
 
     @kernel
     def sqrt_y(self, qubit: TInt32 = -1):
-        self._pulse_mu(self._duration_pi_mu >> 1, self._POW_PI >> 1)
-        self._pulse_mu(self._duration_pi_mu << 1, self._sk1_pi_2_y)
-        self._pulse_mu(self._duration_pi_mu << 1, self._msk1_pi_2_y)
+        self._pulse_mu(self._duration_pi_mu() >> 1, self._POW_PI >> 1)
+        self._pulse_mu(self._duration_pi_mu() << 1, self._sk1_pi_2_y)
+        self._pulse_mu(self._duration_pi_mu() << 1, self._msk1_pi_2_y)
 
     @kernel
     def sqrt_y_dag(self, qubit: TInt32 = -1):
-        self._pulse_mu(self._duration_pi_mu >> 1, self._POW_PI_3_4)
-        self._pulse_mu(self._duration_pi_mu << 1, self._sk1_mpi_2_y)
-        self._pulse_mu(self._duration_pi_mu << 1, self._msk1_mpi_2_y)
+        self._pulse_mu(self._duration_pi_mu() >> 1, self._POW_PI_3_4)
+        self._pulse_mu(self._duration_pi_mu() << 1, self._sk1_mpi_2_y)
+        self._pulse_mu(self._duration_pi_mu() << 1, self._msk1_mpi_2_y)
 
     @kernel
     def h(self, qubit: TInt32 = -1):
         self.sqrt_x_dag()
-        self._pulse_mu(self._duration_pi_mu, self._POW_PI >> 2)
-        self._pulse_mu(self._duration_pi_mu, self._sk1_pi_h)
-        self._pulse_mu(self._duration_pi_mu, self._msk1_pi_h)
+        self._pulse_mu(self._duration_pi_mu(), self._POW_PI >> 2)
+        self._pulse_mu(self._duration_pi_mu(), self._sk1_pi_h)
+        self._pulse_mu(self._duration_pi_mu(), self._msk1_pi_h)
         self.sqrt_x()
