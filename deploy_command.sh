b5cb71e7c6137a3ac0946b5f906b0099f073f596
Paweł <marmeladapk@users.noreply.github.com>
2024-10-30 15:13:21 +0100

    Update master IP

--- deploy_command.sh
+++ deploy_command.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm -rf ~/artiq_tutorial
-./manage deploy --system all --deploy-dir ~/artiq_tutorial/ --users 12 --master-ip 172.31.91.201 --overwrite-system --ctl-host 172.31.91.201 --scope-ctl-host 172.31.91.201
+master_ip=172.31.36.134
+./manage deploy --system all --deploy-dir ~/artiq_tutorial/ --users 6 --master-ip $master_ip --overwrite-system --ctl-host $master_ip --scope-ctl-host $master_ip
