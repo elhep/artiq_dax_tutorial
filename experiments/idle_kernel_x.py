1aeca795ae87e15d1883efb2a2ed446e9eeeea51
Paweł <marmeladapk@users.noreply.github.com>
2024-10-31 13:53:38 +0100

    Updated idle kernel and startup kernel to include fastino

--- experiments/idle_kernel_x.py
+++ experiments/idle_kernel_x.py
@@ -1,53 +1,35 @@
 from artiq.experiment import *
-import numpy as np
-from time import sleep
 
 
 class Test(EnvExperiment):
-    kernel_invariants = {'freqs', 'steps_no', 'urukul_channels'}
+    kernel_invariants = {'urukul_channels'}
     def build(self):
         self.setattr_device("core")
-        self.setattr_device("core_dma")
         
-        self.setattr_device("ttl0")
+        self.setattr_device("ttl1")
+        self.setattr_device("ttl3")
+        self.setattr_device("ttl5") # Input
         self.setattr_device("urukul0_cpld")
         self.setattr_device("urukul0_ch0")
-        self.setattr_device("urukul0_ch1")
         self.urukul_channels = [
-            self.urukul0_ch0,
-            self.urukul0_ch1
+            self.urukul0_ch0
         ]
-        self.setattr_device("phaser0")
-
-        self.steps_no = 20
-        steps = np.linspace(0, 1., self.steps_no)
-        self.freqs = [
-            [(o * 1 + s) * MHz for s in steps]
-            for o in range(5)
-        ]
+        self.setattr_device("fastino0")
 
     @kernel
     def loop_procedure(self):
         for i in range(100):
-            t = (100)
+            t = 100 * us
             with parallel:
-                self.ttl0.pulse(t*us)
-                self.urukul0_ch0.sw.pulse(t*us)
-            self.urukul0_ch1.sw.pulse(t*us)
-
-        # delay(100*ms)
-
-        # for i in range(self.steps_no):
-        #     delay(100*ms)
-        #     with parallel:
-        #         self.ttl0.pulse(100*us)
-        #         for o in range(5):
-        #             self.phaser0.channel[0].oscillator[o].set_frequency(self.freqs[o][i])
-        #             self.phaser0.channel[0].oscillator[o].set_amplitude_phase(.2)
-        #             self.phaser0.channel[1].oscillator[o].set_frequency(-self.freqs[o][i])
-        #             self.phaser0.channel[1].oscillator[o].set_amplitude_phase(.2)
-        #             delay(1*ms)
-
+                self.ttl1.pulse(t)
+                self.ttl3.pulse(t)
+                # Due to switching 10 MHz signal output on oscilloscope may vary
+                self.urukul0_ch0.sw.pulse(t)
+                with sequential:
+                    self.fastino0.set_dac(dac=0, voltage=3*V)
+                    delay(t)
+                    self.fastino0.set_dac(dac=0, voltage=0*V)
+            delay(t)
 
     @kernel
     def run(self):
--- experiments/startup_kernel_x.py
+++ experiments/startup_kernel_x.py
@@ -5,21 +5,22 @@ from time import sleep
 class Test(EnvExperiment):
     def build(self):
         self.setattr_device("core")
-        self.setattr_device("core_dma")
         
-        self.setattr_device("ttl0")
+        self.setattr_device("ttl1")
+        self.setattr_device("ttl3")
+        self.setattr_device("ttl5") # Input
         self.setattr_device("urukul0_cpld")
         self.setattr_device("urukul0_ch0")
-        self.setattr_device("urukul0_ch1")
         self.urukul_channels = [
-            self.urukul0_ch0,
-            self.urukul0_ch1
+            self.urukul0_ch0
         ]
-        self.setattr_device("phaser0")
+        self.setattr_device("fastino0")
 
     @kernel
     def init(self):
         self.core.reset()
+
+        self.ttl5.input()
 
         self.urukul0_cpld.init()
         for urukul_ch in self.urukul_channels:
@@ -27,25 +28,7 @@ class Test(EnvExperiment):
             urukul_ch.sw.off()
             urukul_ch.set_att(31.5)
 
-    @kernel
-    def set_phaser_frequencies(self, phaser, duc, osc):
-        self.core.break_realtime()
-        phaser.init()
-        delay(1*ms)
-        phaser.channel[0].set_duc_frequency(duc)
-        phaser.channel[0].set_duc_cfg()
-        phaser.channel[0].set_att(6*dB)
-        phaser.channel[1].set_duc_frequency(-duc)
-        phaser.channel[1].set_duc_cfg()
-        phaser.channel[1].set_att(6*dB)
-        phaser.duc_stb()
-        delay(1*ms)
-        for i in range(len(osc)):
-            phaser.channel[0].oscillator[i].set_frequency(osc[i])
-            phaser.channel[0].oscillator[i].set_amplitude_phase(.2)
-            phaser.channel[1].oscillator[i].set_frequency(-osc[i])
-            phaser.channel[1].oscillator[i].set_amplitude_phase(.2)
-            delay(1*ms)
+        self.fastino0.init()
 
     @kernel
     def run(self):
@@ -56,11 +39,4 @@ class Test(EnvExperiment):
         self.urukul0_ch0.set_att(10.0)
         # self.urukul0_ch0.sw.on()
 
-        self.urukul0_ch1.set(frequency=20*MHz)
-        self.urukul0_ch1.set_att(10.0)
-        # self.urukul0_ch1.sw.on()
-
-        # Now setup Phaser
-        duc = (1)*10*MHz
-        osc = [i*1*MHz for i in range(5)]
-        self.set_phaser_frequencies(self.phaser0, duc, osc)
+        self.fastino0.set_dac(dac=0, voltage=0*V)
